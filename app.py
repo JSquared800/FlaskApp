9b0f2f70ae0f0a6eda6ca1dcaf561800dc055a8f
JSquared800 <88993206+JSquared800@users.noreply.github.com>
2022-05-14 21:29:10 -0500

    s
    s

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import os
 import matplotlib
 import io
 import matplotlib.pyplot as plt
+import numpy as np
 tokenizer = AutoTokenizer.from_pretrained("FlaskApp/Roberta-Large")
 model = AutoModelForSequenceClassification.from_pretrained("FlaskApp/Roberta-Large", num_labels=3)
 model.load_state_dict(torch.load("FlaskApp/fold1_modelroberta-large_epoch6.pth", map_location=torch.device('cpu')))
@@ -31,9 +32,9 @@ def func1(s):
 	if(s == ""):
 		return
 	client = tweepy.Client(bearer_token='AAAAAAAAAAAAAAAAAAAAAORicgEAAAAAy%2BK6TiiRsjrMVeGz7yTjaM%2B9R%2BM%3DVk5nXSbAU9sJ9Oyd3GnuSdDe30QHSTa0drJNCmTDwq9GP0vfPK')
-	query = s
+	query = s + " finance"
 
-	tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=100)
+	tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=49)
 	ss = ""
 	for tweet in tweets.data:
 		ss+=tweet.text
@@ -41,27 +42,38 @@ def func1(s):
 	print(ss)
 	return PredictTweet(ss.split(" ||| "))
 def PredictTweet(tweet_array):
-    counts = np.zeros(3)
-    for tweet in tweet_array:
-
-        conv_dict = {
-            0 : "positive",
+	
+	fout = open("FlaskApp/static/model_response.txt","w", encoding="utf-8")
+	counts = np.zeros(3)
+	result = ""
+	for tweet in tweet_array:
+		conv_dict = {
+			0 : "positive",
             1 : "neutral",
             2 : "negative"
         }
-
-        tokenized_inputs = tokenizer(tweet, max_length=100, truncation=True, padding="max_length", is_split_into_words=False)
-        tokenized_inputs["input_ids"] = torch.tensor(tokenized_inputs["input_ids"]).unsqueeze(0)
-        tokenized_inputs["attention_mask"] = torch.tensor(tokenized_inputs["attention_mask"]).unsqueeze(0)
-        raw_probas = torch.nn.Softmax(dim=-1)(model(tokenized_inputs["input_ids"], tokenized_inputs["attention_mask"]).logits)
-        class_pred = torch.argmax(raw_probas, dim=-1).item()
-        counts[class_pred] += 1
-    return f"There are {int(counts[0])} positive tweets, {int(counts[1])} neutral tweets, and {int(counts[2])} negative tweets"
+		tokenized_inputs = tokenizer(tweet, max_length=49, truncation=True, padding="max_length", is_split_into_words=False)
+		tokenized_inputs["input_ids"] = torch.tensor(tokenized_inputs["input_ids"]).unsqueeze(0)
+		tokenized_inputs["attention_mask"] = torch.tensor(tokenized_inputs["attention_mask"]).unsqueeze(0)
+		raw_probas = torch.nn.Softmax(dim=-1)(model(tokenized_inputs["input_ids"], tokenized_inputs["attention_mask"]).logits)
+		class_pred = torch.argmax(raw_probas, dim=-1).item()
+		counts[class_pred] += 1
+		result += tweet
+		result += f" | Model Classified This Tweet As {conv_dict[class_pred]}\n"
+	fout.write(result)
+	fout.close()
+	return f"There are {int(counts[0])*2}% positive tweets, {int(counts[1])*2}% neutral tweets, and {int(counts[2])*2}% negative tweets"
 @app.route("/")
 @app.route("/home")
 def home():
 	return render_template("index.html")
-import time
+import io
+import random
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+
 @app.route("/result",methods = ['POST','GET'])
 def result():
 	output = request.form.to_dict()
